2e0dd4d122c681f84fabb8707b85b6035db2a876
YasmimAlves01 <yasmimalvesrodrigues16@gmail.com>
2025-05-10 20:28:28 -0300

    Fluxos de engenharia

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿string escolha;
+
+Console.WriteLine("Fluxo de engenharia");
+
+Console.WriteLine("Esta se movendo? sim ou não");
+escolha = Console.ReadLine()!.ToUpper();
+
+if (escolha == "SIM"){
+    Console.WriteLine("Deveria? sim ou não");
+    escolha = Console.ReadLine()!.ToUpper();
+
+    if(escolha == "SIM")
+    {
+        Console.WriteLine("Otimo");
+    }
+    else{
+        Console.WriteLine("Use fita");
+    }
+
+}
+else {
+    Console.WriteLine("Deveria? sim ou não");
+    escolha = Console.ReadLine()!.ToUpper();
+    if(escolha == "NAO")
+    {
+        Console.WriteLine("otimo");
+    }
+    else
+    {
+        Console.WriteLine("Use WD-40");
+    }
+
+}
+
+
